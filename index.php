fedc6caa5791decc428b7c960dd3738887cfdbb6
akshitanayak7 <97383772+akshitanayak7@users.noreply.github.com>
2023-01-02 12:32:32 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hipsters</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+        <link rel="stylesheet" href="login.css">
+        <link rel="stylesheet" href="./client/home.css">
+        <style>
+           .form-container a button{
+                width:100px;
+                margin-left:30px;
+                background-color:azure;
+            }
+            .form-container a button:hover{
+                background-color:#333;
+            }
+        </style>
+    </head>
+    <body>
+        <section>
+        <div class="form-container">
+
+                <h2 style='text-transform:none;'><img style='height:80px;' src="./images/Logo/logo1.png" class="logo" alt=""></h2>
+
+                <a href='./vendor/admin_login.html'><button class='btn'>Admin Login</button></a>
+
+                <a href='login.html'><button class='btn'>Customer Login</button></a>
+
+        </div>
+     </section>
+    </body>
+</html>
---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+
+$uname=$_GET['username'];
+$password=$_GET['password'];
+
+$conn=new mysqli("localhost","root","","acme");
+
+$sql_obj=mysqli_query($conn,"select * from accounts where username='$uname' and password='$password';");
+$total_rows=mysqli_num_rows($sql_obj);
+
+if($total_rows==0){
+    $_SESSION['login']=false;
+    echo "<h1>Invalid Login Credentials.<h1></br>
+    <a href='login.html'>Login again!</a>";
+    die;
+}
+
+$_SESSION['login']=true;
+header('location:client/home.php');
+
+?>
